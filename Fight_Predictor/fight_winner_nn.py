4fd5456da83c95a830237ff4cdf02cd6cffc462e
11510667 <charles.pierse@ucdconnect.ie>
2019-05-19 18:52:03 +0100

    removed train and test files that now have their own directory

--- Fight_Predictor/fight_winner_nn.py
+++ Fight_Predictor/fight_winner_nn.py
@@ -9,18 +9,19 @@ y_test = np.genfromtxt('model_data/y_test.csv', delimiter=',')
 
 hidden_units1 = 120 #150
 hidden_units2 = 120 #150 for both h1 and h2 works well
+epochs = 550
 # #
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(150,input_dim=102,activation='relu',kernel_initializer='normal'))
+model.add(tf.keras.layers.Dense(hidden_units1,input_dim=102,activation='relu',kernel_initializer='normal'))
 model.add(tf.keras.layers.Dropout(0.6))
 model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Dense(150,activation='relu',kernel_initializer='normal'))
+model.add(tf.keras.layers.Dense(hidden_units2,activation='relu',kernel_initializer='normal'))
 model.add(tf.keras.layers.Dropout(0.6))
 model.add(tf.keras.layers.BatchNormalization())
 model.add(tf.keras.layers.Dense(1,activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer= tf.keras.optimizers.RMSprop(0.0001),metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer= tf.keras.optimizers.Adam(0.0001),metrics=['accuracy'])
 
-model.fit(x_train,y_train, epochs= 350, batch_size= 64,validation_split= 0.1)
+model.fit(x_train,y_train, epochs= epochs, batch_size= 64,validation_split= 0.05)
 
 scores = model.evaluate(x_test,y_test)
 
