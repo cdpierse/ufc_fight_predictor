Fixed scaling issue, building two way prediction

Fight_Predictor/predict.py
@@ -7,9 +7,9 @@ import sys
 import numpy as np
 
 
-class Predict:
+class PreparePredictions:
 
-    def __init__(self, stats_model=None, winner_model=None):
+    def __init__(self):
         # the above params are not yet coded.
         self.get_fighters()
         self.set_feature_names()
@@ -31,8 +31,9 @@ class Predict:
         stats_dir = os.path.join(base_dir, 'Fight_Stats', 'data.npz')
         winner_dir = os.path.join(base_dir, 'Fight_Winner', 'data.npz')
         self.winner_feature_names = np.load(winner_dir)['feature_names']
-        self.stats_feature_names = np.load(stats_dir)['feature_names']
+        self.stats_feature_names = list(np.load(stats_dir)['feature_names'])
         self.stats_target_names = np.load(stats_dir)['fight_stats_columns']
+        self.x_train = np.load(stats_dir)['x_train']
 
     def get_fighter_data(self, fighter_name):
         " Returns matched fighter career stats for a given fighter name from scraped_fighters.csv"
@@ -83,19 +84,50 @@ class Predict:
         else:
             print('fighter_pairs is not of type <list>')
 
+        self.process_stats_df()
+
+        return self.stats_prediction_df
+
     def process_stats_df(self):
-        psp = ProductionStatsProcessor(fight_bouts=self.stats_prediction_df)
+        psp = ProductionStatsProcessor(
+            fight_bouts=self.stats_prediction_df, columns=self.stats_feature_names)
         psp.main()
         self.stats_prediction_df = psp.fight_bouts
-        for col in list(self.stats_prediction_df.columns):
-            if col not in self.stats_feature_names:
-                print(f'column {col} not in predict df')
+
+
+class Predict:
+
+    def __init__(self, fighter_pairs, stats_model, winner_model=None):
+        self.stats_model = stats_model
+        self.fighter_pairs = fighter_pairs
+        self.reversed_pairs = []
+
+    def predict_stats(self, array):
+        return self.stats_model.predict(array)
+
+    def reverse_fight_pairs(self):
+        for pair in self.fighter_pairs:
+            self.reversed_pairs.append((pair[1], pair[0]))
+
+    # predicts stats for both orderings of the df
+    def get_average_predictions(self):
+
+        arr1 = PreparePredictions().create_stats_df(self.fighter_pairs)
+        arr2 = PreparePredictions().create_stats_df(self.reversed_pairs)
+        preds1 = self.predict_stats(arr1)
+        preds2 = self.predict_stats(arr2)
+        print(PreparePredictions().stats_target_names)
+        print(np.max(np.array([preds1, preds2]), axis=0)[0])
 
 
 if __name__ == "__main__":
-    p = Predict()
+    base_dir = os.path.join(os.getcwd(), 'Fight_Predictor', 'Files', 'Models')
+    stats_model = keras.models.load_model(os.path.join(
+        base_dir, 'stats_model.h5'), custom_objects={'r2': r2})
+    # p = PreparePredictions(stats_model)
     fight_pair = [('Conor McGregor', 'Nate Diaz'),
-                  ('Daniel Cormier', 'Conor McGregor'),
-                  ('Jon Jones', 'Daniel Cormier')]
-    p.create_stats_df(fight_pair)
-    p.process_stats_df()
+                  ('Daniel Cormier', 'Jon Jones')]
+    # p.create_stats_df(fight_pair)
+    p = Predict(fight_pair, stats_model)
+    p.reverse_fight_pairs()
+    p.get_average_predictions()

Fight_Predictor/processor.py
@@ -9,7 +9,7 @@ import pandas as pd
 from pandas.api.types import CategoricalDtype
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
-from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
 
 pd.set_option("mode.chained_assignment", None)
 
@@ -303,7 +303,7 @@ class Processor:
     def scale(self):
 
         self.original_values = self.fight_bouts.values
-        self.feature_names = self.fight_bouts.columns
+        self.feature_names = self.fight_bouts.columns.tolist()
 
         def save(scaler):
             file_dir = os.path.join(
@@ -311,7 +311,7 @@ class Processor:
             scaler_name = self.scaler_name + '.pkl'
             joblib.dump(scaler, os.path.join(file_dir, scaler_name))
 
-        scaler = RobustScaler()
+        scaler = StandardScaler()
         scaler.fit(self.fight_bouts)
         self.fight_bouts = scaler.transform(self.fight_bouts)
         save(scaler)
@@ -331,7 +331,7 @@ class Processor:
         save_loc = os.path.join(self.base_dir, 'Data',
                                 'Processed_Data', folder)
         print(f'saving at {save_loc}')
-
+        print(self.feature_names)
         np.savez_compressed(os.path.join(save_loc, 'data'),
                             x_train=self.X_train,
                             y_train=self.y_train,
@@ -395,7 +395,7 @@ class ProductionProcessor(Processor):
     def __init__(self, fight_bouts=None):
         super().__init__()
         self.fight_bouts = fight_bouts
-
+        
     def impute(self):
         imputer_path = os.path.join(
             self.base_dir,
@@ -417,17 +417,14 @@ class ProductionProcessor(Processor):
             self.scaler_name + '.pkl')
         scaler = joblib.load(scaler_path)
         self.fight_bouts = scaler.transform(self.fight_bouts)
-
-    # def main():
-    #     self.impute()
-    #     self.scale()
 
 
 class ProductionStatsProcessor(StatsProcessor):
 
-    def __init__(self, fight_bouts=None):
+    def __init__(self, fight_bouts, columns):
         super().__init__()
         self.fight_bouts = fight_bouts
+        self.columns = columns
 
     def impute(self):
         imputer_path = os.path.join(
@@ -438,6 +435,7 @@ class ProductionStatsProcessor(StatsProcessor):
             self.imputer_name + '.pkl')
         imputer = joblib.load(imputer_path)
         imputed_data = imputer.transform(self.fight_bouts)
+
 
         self.fight_bouts = pd.DataFrame(imputed_data, columns=self.fight_bouts.columns)
 
@@ -450,12 +448,16 @@ class ProductionStatsProcessor(StatsProcessor):
             self.scaler_name + '.pkl')
         scaler = joblib.load(scaler_path)
         self.fight_bouts = scaler.transform(self.fight_bouts)
+    
+    def reindex(self):
+        self.fight_bouts = self.fight_bouts.reindex(columns=self.columns)
 
     def main(self):
         self.drop_unused_columns()
         self.process_categorical_columns(state='production')
+        self.reindex()
         self.impute()
-        #self.scale()
+        self.scale()
 
 
 if __name__ == "__main__":