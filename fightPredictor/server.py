881b07388e27a6af3df65a7ee003b5ec64822700
Charles Pierse <charlespierse@gmail.com>
2019-12-08 14:03:29 +0000

    Add GET request for fighter names

--- fightPredictor/server.py
+++ fightPredictor/server.py
@@ -1,20 +1,20 @@
 import os
 
+import pandas as pd
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from tensorflow import keras
-from utils import r2
+
 from predict import Predict
+from utils import r2
 
 app = Flask(__name__)
 CORS(app)
 BASE_DIR = os.path.join(os.getcwd(), 'fightPredictor', 'Files', 'Models')
 
 
-
 @app.route("/fight-predictor/api/v1.0/predict", methods=['GET'])
 def get_prediction():
-    print(os.getcwd())
     stats_model = keras.models.load_model(os.path.join(
         BASE_DIR, 'stats_model.h5'), custom_objects={'r2': r2})
     winner_model = keras.models.load_model(os.path.join(
@@ -22,39 +22,23 @@ def get_prediction():
     fighter1 = request.args.get('fighter1')
     fighter2 = request.args.get('fighter2')
     prediction_tuple = [(fighter1, fighter2)]
-    print(prediction_tuple)
     p = Predict(prediction_tuple, stats_model, winner_model)
     raw_response = p.predictions[0]
     response = f'{str(raw_response[0]), str(float(raw_response[1]))}'
 
     return response
 
+
 @app.route("/fight-predictor/api/v1.0/fighters", methods=['GET'])
 def get_fighters():
-    pass
+    filedir = os.path.join(
+        'fightPredictor', 'Data',
+        'Scraped_Data', 'scraped_fighters.csv')
+    fighters = pd.read_csv(filedir)
+    fighter_names = list(fighters.fighter_name.unique())
+    return jsonify({'fighter_names': fighter_names})
+
     # code to fetch fighter list
-
-
-
-# @app.route("/fight-predictor/api/v1.0/predict", methods=['GET'])
-# def main():
-#     print(os.getcwd())
-#     base_dir = os.path.join(os.getcwd(), 'fightPredictor', 'Files', 'Models')
-#     stats_model = keras.models.load_model(os.path.join(
-#         base_dir, 'stats_model.h5'), custom_objects={'r2': r2})
-#     winner_model = keras.models.load_model(os.path.join(
-#         base_dir, 'winner_model.h5'))
-#     print(request.json['data'])
-
-#     fight_pair = request.json['data']
-#     print(f'the fight_pair is {fight_pair}')
-#     fighter1, fighter2 = fight_pair['fighter1'], fight_pair['fighter2']
-    # prediction_tuple = [(fighter1, fighter2)]
-    # p = Predict(prediction_tuple, stats_model, winner_model)
-    # raw_response = p.predictions[0]
-    # response = f'{str(raw_response[0]), str(float(raw_response[1]))}'
-#     return response
-
 
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=5000)
--- fightPredictor/test.py
+++ fightPredictor/test.py
@@ -2,21 +2,10 @@ import requests
 
 url = "http://localhost:5000/fight-predictor/api/v1.0/predict"
 
-payload = "{\n    \"data\": {\n        \"fighter1\": \"Joe Lauzon\",\n        \"fighter2\": \"Henry Cejudo\"\n    }\n}"
-#data = {"data": {"fighter1":"Joe Lauzon", "fighter2": "Henry Cejudo"}}
-headers = {
-    'Content-Type': "application/json",
-    'User-Agent': "PostmanRuntime/7.20.1",
-    'Accept': "*/*",
-    'Cache-Control': "no-cache",
-    'Postman-Token': "3640fe30-07e1-4817-b9d7-176926b1b19a,0db4e099-7643-4d4b-accb-d612e73e0d4b",
-    'Host': "localhost:5000",
-    'Accept-Encoding': "gzip, deflate",
-    'Content-Length': "78",
-    'Connection': "keep-alive",
-    'cache-control': "no-cache"
-    }
+querystring = {"fighter1":"Henry Cejudo","fighter2":"Joe Lauzon"}
 
-response = requests.get(url, data = payload, headers = {})
+payload = ""
+
+response = requests.request("GET", url, params=querystring)
 
 print(response.text)
